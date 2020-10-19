09eb30f4a331ebc3bb19782d389cfae9888f5f62
yavitvas <yavv.ukr@gmail.com>
2020-10-19 23:13:55 +0300

    TestCheckWorkDir ok

--- pkg/func_test.go
+++ pkg/func_test.go
@@ -10,7 +10,17 @@ import (
 
 var l = log.New(os.Stdout, "TESTING: ", log.Lshortfile)
 
+func TestCheckWorkDir(t *testing.T) {
+	mydir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	workDir = path.Join(mydir, "testdata")
+	checkWorkDir(l)
+}
+
 func TestWalking(t *testing.T) {
+	exiftoolExist = true
 	mydir, err := os.Getwd()
 	if err != nil {
 		fmt.Println(err)
@@ -20,7 +30,9 @@ func TestWalking(t *testing.T) {
 	fmt.Println(getDateInName, getDateInExif)
 	wantDateInName := 1
 	wantDateInExif := 2
-	if len(getDateInExif) != wantDateInExif || len(getDateInName) != wantDateInName {
-		t.Error("some wrong")
+	if len(getDateInExif) != wantDateInExif {
+		t.Error("Get ", len(getDateInExif), "in DateInExif slice, but want: ", wantDateInExif)
+	} else if len(getDateInName) != wantDateInName {
+		t.Error("Get ", len(getDateInName), "in DateInExif slice, but want: ", wantDateInName)
 	}
 }
